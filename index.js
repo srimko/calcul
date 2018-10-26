77b54a6586e3263f3a1395e14b9499d99a7d9fbe
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2018-10-26 18:11:10 +0200

    Le script ne continue plus après la lancement d'une erreur. TODO : Corriger ce comportement.

--- index.js
+++ index.js
@@ -6,35 +6,7 @@ const _ = require('lodash')
 function ExceptionError (message) {
   this.body = message
   this.name = 'ExceptionUtilisateur'
-}
-
-// Get all parameters
-let parameters = process.argv.slice(2)
-
-const validParameters = ['-montant', '--m']
-
-try {
-  // Check if parameter has a correct syntaxe ex : --m=XXX or -montant=XXX
-  _.each(parameters, (parameter) => {
-    let parameterSplited = parameter.split('=')
-    if (parameterSplited.length > 2 || parameterSplited === undefined) {
-      throw new ExceptionError('Mauvais formattage de paramètre. Pour le moment le seul paramètre supporter est "-montant=100 ou --m=100.')
-    } else {
-      let param = parameterSplited[0]
-      let paramValue = parameterSplited[1]
-
-      // Check if param is a valid parameter
-      let paramExist = _.includes(validParameters, param)
-
-      if (!paramExist) {
-        throw new ExceptionError('Mauvais formattage de paramètre. Pour le moment le seul paramètre supporter est "-montant=100 ou --m=100.')
-      } else {
-        run(paramValue)
-      }
-    }
-  })
-} catch (error) {
-  console.log(chalk.red.bold('Erreur : ') + error.body)
+  process.exit()
 }
 
 function run (montant) {
@@ -64,4 +36,33 @@ function run (montant) {
       )
       console.log(table.toString())
     })
+}
+
+// Get all parameters
+let parameters = process.argv.slice(2)
+
+const validParameters = ['-montant', '--m']
+
+try {
+  // Check if parameter has a correct syntaxe ex : --m=XXX or -montant=XXX
+  _.each(parameters, (parameter) => {
+    let parameterSplited = parameter.split('=')
+    if (parameterSplited.length > 2 || parameterSplited === undefined) {
+      throw new ExceptionError('Mauvais formattage de paramètre. Pour le moment le seul paramètre supporter est "-montant=100 ou --m=100.')
+    } else {
+      let param = parameterSplited[0]
+      let paramValue = parameterSplited[1]
+
+      // Check if param is a valid parameter
+      let paramExist = _.includes(validParameters, param)
+
+      if (!paramExist) {
+        throw new ExceptionError('Mauvais formattage de paramètre. Pour le moment le seul paramètre supporter est "-montant=100 ou --m=100.')
+      } else {
+        run(paramValue)
+      }
+    }
+  })
+} catch (error) {
+  console.log(chalk.red.bold('Erreur : ') + error.body)
 }
